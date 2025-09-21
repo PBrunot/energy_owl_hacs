c014f048c47dcf0a038fcfeafd8f8abed9a54a22
Pascal Brunot <pascal.brunot@gmail.com>
2025-09-21 14:48:01 +0200

    v0.6.15: simplify sensor

--- custom_components/energy_owl/sensor.py
+++ custom_components/energy_owl/sensor.py
@@ -1,7 +1,6 @@
 """Interfaces with the OWL sensors."""
 
 import logging
-from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -10,8 +9,6 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .const import COORDINATOR, DOMAIN
 from .coordinator import OwlDataUpdateCoordinator
 from .current_sensor import OwlCMSensor
-from .historical_current_sensor import OwlHistoricalCurrentSensor
-from .historical_data_pusher import OwlHistoricalDataPusher
 from .historical_data_sensor import OwlHistoricalDataSensor
 
 _LOGGER = logging.getLogger(__name__)
@@ -25,14 +22,9 @@ async def async_setup_entry(
     """Set up the Sensors."""
     coordinator: OwlDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
 
-    # Create historical current sensor first so pusher can reference it
-    historical_current_sensor = OwlHistoricalCurrentSensor(coordinator, config_entry)
-
     sensors = [
         OwlCMSensor(coordinator, config_entry),
         OwlHistoricalDataSensor(coordinator, config_entry),
-        historical_current_sensor,
-        OwlHistoricalDataPusher(coordinator, config_entry, historical_current_sensor),
     ]
 
     async_add_entities(sensors)
